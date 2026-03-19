f646257f5933b0fc9b01bb52b12ed5792a283e05
leejn <scarecrowlee2@gmail.com>
2026-03-19 11:42:09 +0900

    first commit

---
+++ src/main/java/com/dwoffice/monitoringpjt/MonitoringpjtApplication.java
@@ -0,0 +1,13 @@
+package com.dwoffice.monitoringpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringpjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringpjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwoffice/monitoringpjt/MonitoringpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwoffice.monitoringpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringpjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
